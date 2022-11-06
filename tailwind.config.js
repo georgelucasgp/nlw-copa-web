0084ab7c487c38535ec873b42404351bb521a238
georgelucasgp <djgeorgelucas@hotmail.com>
2022-11-05 23:53:55 -0300

    finish layout web aula 02

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,23 @@ module.exports = {
       fontFamily: {
         sans: "Roboto, sans-serif",
       },
+      backgroundImage: {
+        app: "url(/app-bg.png)",
+      },
       colors: {
+        ignite: {
+          500: "#129E57",
+        },
+        yellow: {
+          500: "#F7DD43",
+          700: "#E5CD3D",
+        },
         gray: {
           900: "#121214",
+          800: "#202024",
+          600: "#323238",
+          300: "#8D8D99",
+          100: "#E1E1E6",
         },
       },
     },
